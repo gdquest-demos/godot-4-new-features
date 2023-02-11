3a368bd7e17c42a341391b2741cc30d3b923fea2
Răzvan C. Rădulescu <razcore.rad@gmail.com>
2023-02-11 19:13:33 +0200

    feat(2d_physics_benchmark): spawn every frame

--- 2d_physics_benchmark/2d_physics_benchmark.gd
+++ 2d_physics_benchmark/2d_physics_benchmark.gd
@@ -2,22 +2,29 @@ extends Node2D
 
 
 const RigidBody := preload("rigid_body_2d.tscn")
+const SPAWNS_INIT := 4000
 
-var rng := RandomNumberGenerator.new()
+var spawns := 0
 
-@onready var fps_label := $FPSLabel
+@onready var label := $Label
+@onready var marker := $Marker2D
 
 
 func _ready() -> void:
-	for idx in range(4000):
-		var rigid_body := RigidBody.instantiate()
-		rigid_body.position = Vector2(20, 0) * index_to_xy(1880, idx)
-		add_child(rigid_body)
+	for idx in range(SPAWNS_INIT):
+		spawn(Vector2(20, 0) * index_to_xy(1880, idx))
 
 
 func _process(delta: float) -> void:
-	fps_label.text = "FPS %d" % [Performance.get_monitor(Performance.TIME_FPS)]
+	label.text = "FPS %d\nSpawns %d" % [Performance.get_monitor(Performance.TIME_FPS), spawns]
+	spawn(marker.position)
 
+
+func spawn(location: Vector2) -> void:
+	var rigid_body := RigidBody.instantiate()
+	rigid_body.position = location
+	add_child(rigid_body)
+	spawns += 1
 
 func index_to_xy(width: int, index: int) -> Vector2:
 	return Vector2(index % width, index / width)
