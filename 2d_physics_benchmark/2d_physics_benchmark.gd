8402347647d5ce160c80dc740048ff2823104608
Răzvan C. Rădulescu <razcore.rad@gmail.com>
2023-02-12 18:12:31 +0200

    feat(2d_physics_benchmark): remove cannons and clean up

--- 2d_physics_benchmark/2d_physics_benchmark.gd
+++ 2d_physics_benchmark/2d_physics_benchmark.gd
@@ -1,37 +1,38 @@
 extends Node2D
 
 
-const CannonBall := preload("cannon_ball.tscn")
+const Ball := preload("ball.tscn")
 
 @export var init_spawns := 1000
+@export var grid_spawns_offset := Vector2(180, 980)
 
+var rng := RandomNumberGenerator.new()
 var spawns := 0
 
 @onready var label := $Label
-@onready var cannons := $Cannons
 
 
 func _ready() -> void:
-	for cannon in cannons.get_children():
-		cannon.connect("fired", increment_spawns)
-
-	for idx in range(init_spawns):
-		spawn(20 * index_to_xy(80, idx) + Vector2(180, 400))
+	rng.randomize()
+	grid_spawn()
 
 
 func _process(_delta: float) -> void:
+	spawn(Vector2(960, 100))
 	label.text = "FPS %d\nSpawns %d" % [Performance.get_monitor(Performance.TIME_FPS), spawns]
 
 
-func increment_spawns() -> void:
-	spawns += 1
+func grid_spawn() -> void:
+	for idx in range(init_spawns):
+		var offset := grid_spawns_offset + Vector2(rng.randf_range(-5, 5), rng.randf_range(-5, 5))
+		spawn(30 * index_to_xy(50, idx).reflect(Vector2.RIGHT) + offset)
 
 
 func spawn(location: Vector2) -> void:
-	var cannon_ball := CannonBall.instantiate()
-	cannon_ball.position = location
-	add_child(cannon_ball)
-	increment_spawns()
+	var ball := Ball.instantiate()
+	ball.position = location
+	add_child(ball)
+	spawns += 1
 
 
 func index_to_xy(width: int, index: int) -> Vector2:
--- 2d_physics_benchmark/ball.gd
+++ 2d_physics_benchmark/ball.gd
@@ -3,16 +3,19 @@ extends RigidBody2D
 
 const ALTERNATE_TEXTURE := preload("assets/cannon_ball_red.png")
 
-var direction := Vector2.ZERO
 var rng := RandomNumberGenerator.new()
 
 @onready var sprite := $Sprite
 
 
 func _ready() -> void:
+	set_as_top_level(true)
 	rng.randomize()
 	if rng.randf() > 0.5:
 		sprite.texture = ALTERNATE_TEXTURE
-	set_as_top_level(true)
-	linear_velocity = 1500 * direction
-	look_at(direction)
+
+	var tween := create_tween()
+	scale = Vector2.ONE * rng.randf_range(0, 0.5)
+	tween.tween_property(self, "scale", Vector2.ONE, 0.4)
+
+	apply_impulse(50 * Vector2.UP.rotated(rng.randf_range(-PI, PI) / 4))
