d3b5d4ae0eea9bae6c5c1d266e97ab8f43b513eb
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2023-02-15 17:23:34 +0100

    refactor: minor code cleanup

--- main/ui_scene_entry.gd
+++ main/ui_scene_entry.gd
@@ -5,18 +5,4 @@ extends Button
 
 
 func _ready():
-	focus_entered.connect(_on_focus_entered)
-	focus_exited.connect(_on_focus_exited)
-	mouse_entered.connect(_on_mouse_entered)
-
-
-func _on_mouse_entered() -> void:
-	grab_focus()
-
-
-func _on_focus_entered() -> void:
-	pass
-
-
-func _on_focus_exited() -> void:
-	pass
+	mouse_entered.connect(grab_focus)
--- main/ui_scene_selector.gd
+++ main/ui_scene_selector.gd
@@ -1,29 +1,31 @@
 extends CanvasLayer
 
 const DEMOS := [
-	{"scene_path": "res://2d_clipping/2d_clipping.tscn", "name": "2D Clipping"},
-	{"scene_path": "res://3d_physics_nodes/3d_physics_nodes.tscn", "name": "3D Physics Nodes"},
-	{"scene_path": "res://animation_retargeting/animation_retargeting.tscn", "name": "Animation Retargeting"},
-	{"scene_path": "res://cutout_character/cutout_character.tscn", "name": "Cutout Character"},
-	{"scene_path": "res://interior-diorama/interior_diorama.tscn", "name": "Interior Diorama"},
-	{"scene_path": "res://outdoor_environment/outdoor_environment.tscn", "name": "Outdoor Environment"},
-	{"scene_path": "res://theme_variations/theme_variations.tscn", "name": "Theme Variations"},
-	{"scene_path": "res://tilemap/tilemap_based_level.tscn", "name": "Tilemap Level"},
-	{"scene_path": "res://tweens/tween_demo.tscn", "name": "Tween Demo"},
-	{"scene_path": "res://ui_flexbox/flow_container_demo.tscn", "name": "Flow Container"}
+	{"path": "res://2d_clipping/2d_clipping.tscn", "name": "2D Clipping"},
+	{"path": "res://3d_physics_nodes/3d_physics_nodes.tscn", "name": "3D Physics Nodes"},
+	{"path": "res://animation_retargeting/animation_retargeting.tscn", "name": "Animation Retargeting"},
+	{"path": "res://cutout_character/cutout_character.tscn", "name": "Cutout Character"},
+	{"path": "res://interior-diorama/interior_diorama.tscn", "name": "Interior Diorama"},
+	{"path": "res://outdoor_environment/outdoor_environment.tscn", "name": "Outdoor Environment"},
+	{"path": "res://theme_variations/theme_variations.tscn", "name": "Theme Variations"},
+	{"path": "res://tilemap/tilemap_based_level.tscn", "name": "Tilemap Level"},
+	{"path": "res://tweens/tween_demo.tscn", "name": "Tween Demo"},
+	{"path": "res://ui_flexbox/flow_container_demo.tscn", "name": "Flow Container"}
 ]
 const UI_SCENE_ENTRY_SCENE := preload("res://main/ui_scene_entry.tscn")
 
 @onready var grid_container : GridContainer = $SelectorRoot/GridContainer
 @onready var selector_root : Control = $SelectorRoot
 
+@onready var scene_tree := get_tree()
+
 @onready var _cached_mouse_mode : int
-@onready var _current_scene_idx : int
+@onready var _current_scene_index : int
 
 
 func _ready():
-	if get_tree().current_scene == self:
-		get_tree().current_scene = null
+	if scene_tree.current_scene == self:
+		scene_tree.current_scene = null
 	
 	for i in range(DEMOS.size()):
 		var entry : Button = UI_SCENE_ENTRY_SCENE.instantiate()
@@ -38,20 +40,20 @@ func _ready():
 func _on_entry_pressed(demo_id: int) -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	_cached_mouse_mode = Input.MOUSE_MODE_VISIBLE
-	get_tree().change_scene_to_file(DEMOS[demo_id]["scene_path"])
-	_current_scene_idx = demo_id
+	scene_tree.change_scene_to_file(DEMOS[demo_id]["path"])
+	_current_scene_index = demo_id
 	resume()
 
 
-func _input(event):
+func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
-		if get_tree().current_scene == null:
+		if scene_tree.current_scene == null:
 			return
 		if visible:
 			resume()
 		else:
 			pause()
-			grid_container.get_child(_current_scene_idx).grab_focus()
+			grid_container.get_child(_current_scene_index).grab_focus()
 
 
 func pause() -> void:
@@ -60,8 +62,8 @@ func pause() -> void:
 	var tween := create_tween()
 	tween.tween_property(selector_root, "modulate", Color.WHITE, 0.2)
 	
-	if get_tree().current_scene:
-		get_tree().current_scene.process_mode = Node.PROCESS_MODE_DISABLED
+	if scene_tree.current_scene != null:
+		scene_tree.current_scene.process_mode = Node.PROCESS_MODE_DISABLED
 		_cached_mouse_mode = Input.mouse_mode
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
@@ -72,6 +74,6 @@ func resume() -> void:
 	tween.tween_property(selector_root, "modulate", Color.TRANSPARENT, 0.2)
 	tween.tween_callback(set_visible.bind(false))
 	
-	if get_tree().current_scene:
-		get_tree().current_scene.process_mode = Node.PROCESS_MODE_ALWAYS
+	if scene_tree.current_scene:
+		scene_tree.current_scene.process_mode = Node.PROCESS_MODE_ALWAYS
 		Input.mouse_mode = _cached_mouse_mode
