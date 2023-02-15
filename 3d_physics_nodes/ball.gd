ce7efe627bc4997668c088ac11780a96b5aad208
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2023-02-15 16:44:31 +0100

    feat: add playground with multiple 3D physics nodes

--- 3d_physics_nodes/ball.gd
+++ 3d_physics_nodes/ball.gd
@@ -2,7 +2,3 @@ extends RigidBody3D
 
 @onready var mesh_instance: MeshInstance3D = $MeshInstance3D
 
-
-func _ready():
-	# Randomize the color of each ball instance
-	mesh_instance.set("instance_shader_parameters/albedo", Color.from_hsv(randf_range(0.0, 0.99), 0.5, 0.5))
