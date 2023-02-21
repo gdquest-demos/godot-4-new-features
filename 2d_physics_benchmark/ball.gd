4b127ae9265a1b00d5b6549dbf874adbd0459c7a
Tibo <goiffonthibaud@protonmail.com>
2023-02-21 15:40:03 +0100

    fix: ball mat

--- 2d_physics_benchmark/ball.gd
+++ 2d_physics_benchmark/ball.gd
@@ -16,6 +16,6 @@ func _ready() -> void:
 	var base_scale = sprite.scale
 	tween.tween_property(sprite, "scale", base_scale, 0.4).from(Vector2.ZERO)
 	
-	sprite.material.set("shader_parameter/color", gradient.sample(randf()))
+	sprite.modulate = gradient.sample(randf())
 	
 	apply_impulse(50 * Vector2.UP.rotated(rng.randf_range(-PI, PI) / 4))
