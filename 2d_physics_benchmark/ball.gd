2bf14fd6b010021a9f99a4b7dbf60dd8fb47e31b
Tibo <goiffonthibaud@protonmail.com>
2023-02-21 12:37:49 +0100

    update: 2DPhysicsBenchmark visual

--- 2d_physics_benchmark/ball.gd
+++ 2d_physics_benchmark/ball.gd
@@ -6,16 +6,16 @@ const ALTERNATE_TEXTURE := preload("assets/cannon_ball_red.png")
 var rng := RandomNumberGenerator.new()
 
 @onready var sprite := $Sprite
-
+@export var gradient : Gradient
 
 func _ready() -> void:
-	set_as_top_level(true)
 	rng.randomize()
-	if rng.randf() > 0.5:
-		sprite.texture = ALTERNATE_TEXTURE
 
 	var tween := create_tween()
-	scale = Vector2.ONE * rng.randf_range(0, 0.5)
-	tween.tween_property(self, "scale", Vector2.ONE, 0.4)
-
+	
+	var base_scale = sprite.scale
+	tween.tween_property(sprite, "scale", base_scale, 0.4).from(Vector2.ZERO)
+	
+	sprite.material.set("shader_parameter/color", gradient.sample(randf()))
+	
 	apply_impulse(50 * Vector2.UP.rotated(rng.randf_range(-PI, PI) / 4))
