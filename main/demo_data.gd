fe4e7a0433dc0d81953b2e2d4259a9515bafd2bd
Xananax <me@xananax.com>
2023-02-26 18:52:12 +0400

    chore: add almost all demos

--- main/demo_data.gd
+++ main/demo_data.gd
@@ -5,6 +5,7 @@ class_name DemoData
 @export var thumbnail : Texture2D
 @export_file var scene_path
 
+
 static func setup(initial_title: String, intial_texture: Texture2D, initial_path: String) -> DemoData:
 	var data := DemoData.new()
 	data.title = initial_title
--- main/ui_scene_selector.gd
+++ main/ui_scene_selector.gd
@@ -7,13 +7,114 @@ extends Node
 @onready var _current_scene_index : int
 
 var DEMOS : Array[DemoData] = [
-	DemoData.setup("2d_clipping", preload("./thumbnails/2d_clipping_thumbnail.png"), preload("res://2d_clipping/2d_clipping.tscn").resource_path),
-	DemoData.setup("animation_retargeting", preload("./thumbnails/animation_retargeting_thumbnail.png"), preload("res://animation_retargeting/animation_retargeting.tscn").resource_path),
-	DemoData.setup("Ball Pool", preload("./thumbnails/ball_pool_thumbnail.png"), preload("res://3d_balls_pool/3d_balls_pool.tscn").resource_path),
-	DemoData.setup("canvas_group", preload("./thumbnails/canvas_group_thumbnail.png"), preload("res://cutout_character/cutout_character.tscn").resource_path),
-	DemoData.setup("heightmap", preload("./thumbnails/heightmap_thumbnail.png"), preload("res://heightmap_physics/heightmap_physics.tscn").resource_path),
-	DemoData.setup("multilingual", preload("./thumbnails/multilingual_thumbnail.png"), preload("res://dialogue_tree/dialogue_tree_ui.tscn").resource_path),
-	DemoData.setup("voxelgi", preload("./thumbnails/voxelgi_thumbnail.png"), preload("res://interior-diorama/interior_diorama.tscn").resource_path),
+	DemoData.setup(
+		"2d_clipping", 
+		preload("./thumbnails/2d_clipping_thumbnail.png"), 
+		preload("res://2d_clipping/2d_clipping.tscn").resource_path
+	),
+	DemoData.setup(
+		"2D Dynamic Lighting", 
+		preload("./thumbnails/2d_dynamic_lights.png"), 
+		preload("res://2d_dynamic_lights/2d_dynamic_lights.tscn").resource_path
+	),
+	DemoData.setup(
+		"2D Lighting Normal Map", 
+		preload("./thumbnails/2d_lighting_normal_map.png"), 
+		preload("res://2d_lighting_normal_map/2d_lighting_normal_map.tscn").resource_path
+	),
+	DemoData.setup(
+		"2D Particles", 
+		preload("./thumbnails/2d_particles.png"), 
+		preload("res://2d_particles/rain.tscn").resource_path
+	),
+	DemoData.setup(
+		"2D Physics Benchmark", 
+		preload("./thumbnails/2d_physics_benchmark.png"), 
+		preload("res://2d_physics_benchmark/2d_physics_benchmark.tscn").resource_path
+	),
+	DemoData.setup(
+		"3D Animation Tree Audio", 
+		preload("./thumbnails/3d_animation_tree_audio.png"), 
+		preload("res://3d_animation_tree_audio/3d_animation_tree_audio.tscn").resource_path
+	),
+	DemoData.setup(
+		"3D Balls Pool", 
+		preload("./thumbnails/ball_pool_thumbnail.png"), 
+		preload("res://3d_balls_pool/3d_balls_pool.tscn").resource_path
+	),
+	# TODO: set up 3D particles scene
+	#DemoData.setup(
+	#	"3D Particles", 
+	#	preload("./thumbnails/.png"), 
+	#	preload("res://3d_balls_pool/3d_balls_pool.tscn").resource_path
+	#),
+	DemoData.setup(
+		"3D Physics Nodes", 
+		preload("./thumbnails/3d_physics_nodes.png"), 
+		preload("res://3d_physics_nodes/3d_physics_nodes.tscn").resource_path
+	),
+	DemoData.setup(
+		"animation_retargeting", 
+		preload("./thumbnails/animation_retargeting_thumbnail.png"), 
+		preload("res://animation_retargeting/animation_retargeting.tscn").resource_path
+	),
+	DemoData.setup(
+		"Audio Polyphony", 
+		preload("./thumbnails/audio_polyphony.png"),
+		preload("res://audio_polyphony/audio_polyphony.tscn").resource_path
+	),
+	DemoData.setup(
+		"Canvas Group", 
+		preload("./thumbnails/canvas_group_thumbnail.png"), 
+		preload("res://cutout_character/cutout_character.tscn").resource_path
+	),
+	DemoData.setup(
+		"Multilingual", 
+		preload("./thumbnails/multilingual_thumbnail.png"), 
+		preload("res://dialogue_tree/dialogue_tree_ui.tscn").resource_path
+	),
+	
+	DemoData.setup(
+		"Heightmap Physics", 
+		preload("./thumbnails/heightmap_thumbnail.png"), 
+		preload("res://heightmap_physics/heightmap_physics.tscn").resource_path
+	),
+	DemoData.setup(
+		"Voxel GI", 
+		preload("./thumbnails/voxelgi_thumbnail.png"), 
+		preload("res://interior-diorama/interior_diorama.tscn").resource_path
+	),
+	# TODO: verify why this isn't working
+	# DemoData.setup(
+	# 	"Networking", 
+	# 	preload("./thumbnails/.png"), 
+	# 	preload("res://networking/robot_war/robot_war.tscn").resource_path
+	# ),
+	DemoData.setup(
+		"Environment", 
+		preload("./thumbnails/environment.png"), 
+		preload("res://outdoor_environment/outdoor_environment.tscn").resource_path
+	),
+	DemoData.setup(
+		"Theme Variations", 
+		preload("./thumbnails/theme_variations.png"), 
+		preload("res://theme_variations/theme_variations.tscn").resource_path
+	),
+	DemoData.setup(
+		"Tilemap", 
+		preload("./thumbnails/tilemap.png"), 
+		preload("res://tilemap/tilemap_based_level.tscn").resource_path
+	),
+	DemoData.setup(
+		"Tweens", 
+		preload("./thumbnails/tween.png"), 
+		preload("res://tweens/tween_demo.tscn").resource_path
+	),
+	DemoData.setup(
+		"Flow Container", 
+		preload("./thumbnails/flow_container.png"), 
+		preload("res://ui_flexbox/flow_container_demo.tscn").resource_path
+	),
 ]
 
 func _ready() -> void:
