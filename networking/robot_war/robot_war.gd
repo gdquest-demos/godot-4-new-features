fe2be436089299e524d624645b7f3cd33d054000
Xananax <me@xananax.com>
2023-02-26 18:52:25 +0400

    fix: make sure window is reachable

--- networking/robot_war/robot_war.gd
+++ networking/robot_war/robot_war.gd
@@ -6,9 +6,12 @@ const GAME_WINDOW_SCENE := preload("res://networking/robot_war/robot_war_window.
 
 
 func _ready() -> void:
-	spawn_window_button.pressed.connect(func():
-		var window := GAME_WINDOW_SCENE.instantiate()
-		add_child(window))
+	spawn_window_button.pressed.connect(
+		func():
+			var window := GAME_WINDOW_SCENE.instantiate()
+			add_child(window)
+			window.popup_centered()
+	)
 
 
 func _unhandled_key_input(event):
