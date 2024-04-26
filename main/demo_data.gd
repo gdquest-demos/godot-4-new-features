985b5cb69f6cd2160def6a4fd12817b0b9a760c0
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2024-04-26 15:22:58 +0200

    improvement: load scenes as needed, speeding up initial load

--- main/demo_data.gd
+++ main/demo_data.gd
@@ -1,12 +1,14 @@
 extends Resource
 class_name DemoData
 
-@export var title : String = ""
-@export var thumbnail : Texture2D
+@export var title := ""
+@export var thumbnail: Texture2D = null
 @export_file var scene_path := ""
 @export_multiline var description := ""
 
 static func setup(initial_title: String, intial_texture: Texture2D, initial_path: String, initial_description: String) -> DemoData:
+	assert(FileAccess.file_exists(initial_path), "The scene at path %s was not found." % initial_path)
+
 	var data := DemoData.new()
 	data.title = initial_title
 	data.thumbnail = intial_texture
--- main/ui_scene_selector.gd
+++ main/ui_scene_selector.gd
@@ -12,188 +12,188 @@ var DEMOS : Array[DemoData] = [
 	DemoData.setup(
 		"Particles Turbulence",
 		preload("./thumbnails/particles_turbulence.png"),
-		preload("res://particles_turbulence/main_particles_turbulence.tscn").resource_path,
+		"res://particles_turbulence/main_particles_turbulence.tscn",
 		"Godot 4.1's improved particles turbulence"
 	),
 	DemoData.setup(
 		"2D clipping",
 		preload("./thumbnails/2d_clipping_thumbnail.png"),
-		preload("res://2d_clipping/2d_clipping_background.tscn").resource_path,
+		"res://2d_clipping/2d_clipping_background.tscn",
 		"Demonstrates how you can use a texture to clip parts of another"
 	),
 	DemoData.setup(
 		"2D Dynamic Lighting",
 		preload("./thumbnails/2d_dynamic_lights.png"),
-		preload("res://2d_dynamic_lights/2d_dynamic_lights.tscn").resource_path,
+		"res://2d_dynamic_lights/2d_dynamic_lights.tscn",
 		"Shows an animated 2D light projecting shadows"
 	),
 	DemoData.setup(
 		"2D Lighting Normal Map",
 		preload("./thumbnails/2d_lighting_normal_map_thumbnail.png"),
-		preload("res://2d_lighting_normal_map/2d_lighting_normal_map.tscn").resource_path,
+		"res://2d_lighting_normal_map/2d_lighting_normal_map.tscn",
 		"Demonstrates a directional 2D light creating 2D shadows in real time"
 	),
 	DemoData.setup(
 		"2D Particles",
 		preload("./thumbnails/2d_particles_thumbnail.png"),
-		preload("res://2d_particles/rainy_night.tscn").resource_path,
+		"res://2d_particles/rainy_night.tscn",
 		"Particles used to create rain that falls and bounces"
 	),
 	DemoData.setup(
 		"2D Physics Benchmark",
 		preload("./thumbnails/2d_physics_benchmark.png"),
-		preload("res://2d_physics_benchmark/2d_physics_benchmark.tscn").resource_path,
+		"res://2d_physics_benchmark/2d_physics_benchmark.tscn",
 		"Spawns plenty of round shapes to test physics"
 	),
 	DemoData.setup(
 		"2D Skeleton",
 		preload("./thumbnails/2d_skeleton_thumbnail.png"),
-		preload("res://2d_skeleton/2d_skeleton.tscn").resource_path,
+		"res://2d_skeleton/2d_skeleton.tscn",
 		"Create simple rig with the modification stack"
 	),
 	DemoData.setup(
 		"Auto LOD",
 		preload("./thumbnails/lod_thumbnail.png"),
-		preload("res://LOD/lod_demo.tscn").resource_path,
+		"res://LOD/lod_demo.tscn",
 		"Reduces 3D model detail based on camera distance for performance benefits."
 	),
 	DemoData.setup(
 		"3D Animation Tree Audio",
 		preload("./thumbnails/3d_animation_tree_audio.png"),
-		preload("res://3d_animation_tree_audio/3d_animation_tree_audio.tscn").resource_path,
+		"res://3d_animation_tree_audio/3d_animation_tree_audio.tscn",
 		"Control Sofia with WASD: Demonstrates how sounds easily overlap now"
 	),
 	DemoData.setup(
 		"3D Balls Pool",
 		preload("./thumbnails/ball_pool_thumbnail.png"),
-		preload("res://3d_balls_pool/3d_balls_pool.tscn").resource_path,
+		"res://3d_balls_pool/3d_balls_pool.tscn",
 		"Click to spawn 3D balls in this 3D physics benchmark"
 	),
 	DemoData.setup(
 		"3D Physics Nodes",
 		preload("./thumbnails/3d_physics_nodes.png"),
-		preload("res://3d_physics_nodes/3d_physics_nodes.tscn").resource_path,
+		"res://3d_physics_nodes/3d_physics_nodes.tscn",
 		"Move with WASD and push physics balls around"
 	),
 	DemoData.setup(
 		"3D Particles",
 		preload("./thumbnails/3d_particles_thumbnail.png"),
-		preload("res://3d_particles/rain.tscn").resource_path,
+		"res://3d_particles/rain.tscn",
 		"A simple scene showing the new 3D particle physics. [Click] to generate a new mountain."
 	),
 	DemoData.setup(
 		"Animation retargeting",
 		preload("./thumbnails/animation_retargeting_thumbnail.png"),
-		preload("res://animation_retargeting/animation_retargeting.tscn").resource_path,
+		"res://animation_retargeting/animation_retargeting.tscn",
 		"3 completely different meshes dance for you"
 	),
 	DemoData.setup(
 		"Navigation",
 		preload("./thumbnails/navigation_1.png"),
-		preload("res://navigation/navigation_arena.tscn").resource_path,
+		"res://navigation/navigation_arena.tscn",
 		"Click to move the beetle with navigation mesh in real time"
 	),
 	DemoData.setup(
 		"Dynamic Avoidance",
 		preload("./thumbnails/navigation_obstacles.png"),
-		preload("res://navigation/navigation_obstacles.tscn").resource_path,
+		"res://navigation/navigation_obstacles.tscn",
 		"Click to move the beetle and see it avoid the balls with navigating mesh in real time"
 	),
 	DemoData.setup(
 		"Dynamic Paths",
 		preload("./thumbnails/navigation_2.png"),
-		preload("res://navigation/navigation_moving_platforms.tscn").resource_path,
+		"res://navigation/navigation_moving_platforms.tscn",
 		"Those beetles patiently wait for their path to clear up to go to their destination"
 	),
 	DemoData.setup(
 		"Avoidance Priority",
 		preload("./thumbnails/navigation_priority.png"),
-		preload("res://navigation/navigation_priority.tscn").resource_path,
+		"res://navigation/navigation_priority.tscn",
 		"See how those big and angry beetles bully the tiny ones because of their higher avoidance priority"
 	),
 	DemoData.setup(
 		"Navigation Teleportation via Links",
 		preload("./thumbnails/navigation_teleport.png"),
-		preload("res://navigation/navigation_teleport.tscn").resource_path,
+		"res://navigation/navigation_teleport.tscn",
 		"Watch the beetle get teleported to the other island with the help of navigation links"
 	),
 	DemoData.setup(
 		"Audio Polyphony",
 		preload("./thumbnails/audio_polyphony.png"),
-		preload("res://audio_polyphony/audio_polyphony.tscn").resource_path,
+		"res://audio_polyphony/audio_polyphony.tscn",
 		"Test the new max polyphony capacity of AudioStreamPlayer nodes"
 	),
 	DemoData.setup(
 		"Canvas Group",
 		preload("./thumbnails/canvas_group_thumbnail.png"),
-		preload("res://cutout_character/cutout_character.tscn").resource_path,
+		"res://cutout_character/cutout_character.tscn",
 		"A group of nodes fades out; the one using canvas groups shows no overlaps"
 	),
 	DemoData.setup(
 		"Multilingual",
 		preload("./thumbnails/multilingual_thumbnail.png"),
-		preload("res://dialogue_tree/dialogue_tree_ui.tscn").resource_path,
+		"res://dialogue_tree/dialogue_tree_ui.tscn",
 		"Check how unicode characters display properly in UI nodes and buttons; You can also scale the font dynamically"
 	),
 
 	DemoData.setup(
 		"Heightmap Physics",
 		preload("./thumbnails/heightmap_thumbnail.png"),
-		preload("res://heightmap_physics/heightmap_physics.tscn").resource_path,
+		"res://heightmap_physics/heightmap_physics.tscn",
 		"[Click] to generate a random mountain, and press [SPACE] to drop balls. Demonstrates how heightmaps can affect physics"
 	),
 	DemoData.setup(
 		"Voxel GI",
 		preload("./thumbnails/voxelgi_thumbnail.png"),
-		preload("res://interior-diorama/interior_diorama.tscn").resource_path,
+		"res://interior-diorama/interior_diorama.tscn",
 		"Rotate the room around. A simple setup to demonstrate the results that can be obtained using VoxelGI"
 	),
 	DemoData.setup(
 		"Networking",
 		preload("./thumbnails/networking_robot_war_thumbnails.png"),
-		preload("res://networking/robot_war/robot_war.tscn").resource_path,
+		"res://networking/robot_war/robot_war.tscn",
 		"A local multiplayer demo. Control the robot with [WASD] keys, jump with [SPACE] and punch with the Left Mouse button"
 	),
 	DemoData.setup(
 		"Environment",
 		preload("./thumbnails/environment.png"),
-		preload("res://outdoor_environment/outdoor_environment.tscn").resource_path,
+		"res://outdoor_environment/outdoor_environment.tscn",
 		"Outdoor scene example showing a full 3D environment and simple character controller"
 	),
 	DemoData.setup(
 		"Theme Variations",
 		preload("./thumbnails/theme_variations.png"),
-		preload("res://theme_variations/theme_variations.tscn").resource_path,
+		"res://theme_variations/theme_variations.tscn",
 		"The scene isn't interactive, but you can see how it works by opening it in the editor"
 	),
 	DemoData.setup(
 		"Tilemap",
 		preload("./thumbnails/tilemap.png"),
-		preload("res://tilemap/tilemap_based_level.tscn").resource_path,
+		"res://tilemap/tilemap_based_level.tscn",
 		"An example of a tilemap scene. Use WASD to move the character around"
 	),
 	DemoData.setup(
 		"Tweens",
 		preload("./thumbnails/tweens_thumbnail.png"),
-		preload("res://tweens/tween_demo.tscn").resource_path,
+		"res://tweens/tween_demo.tscn",
 		"Click somewhere to move the ship and collect the rupees"
 	),
 	DemoData.setup(
 		"Flow Container",
 		preload("./thumbnails/flow_container.png"),
-		preload("res://ui_flexbox/flow_container_demo.tscn").resource_path,
+		"res://ui_flexbox/flow_container_demo.tscn",
 		"Add or remove items and see how they neatly stack next to each other dynamically"
 	),
 	DemoData.setup(
 		"Exported Node Array",
 		preload("./thumbnails/exported_node_array.png"),
-		preload("res://exported_node_array/exported_node_array.tscn").resource_path,
+		"res://exported_node_array/exported_node_array.tscn",
 		"Click on the buttons to find out the correct combination to open the door"
 	),
 	DemoData.setup(
 		"Info & Credits",
 		preload("./thumbnails/thanks.png"),
-		preload("res://main/thanks/thanks.tscn").resource_path,
+		"res://main/thanks/thanks.tscn",
 		"Some things that we couldn't include, credits, licensing"
 	),
 ]
@@ -209,8 +209,8 @@ func _ready() -> void:
 	main_menu.card_selector.grid.get_child(0).grab_focus()
 
 
-const HintText = preload("res://main/menu_scene_selector/hint_text.gd")
-const HintTextScene = preload("res://main/menu_scene_selector/hint_text.tscn")
+const HintText := preload("res://main/menu_scene_selector/hint_text.gd")
+const HintTextScene := preload("res://main/menu_scene_selector/hint_text.tscn")
 
 
 func _get_card_hints() -> Array[HintText]:
